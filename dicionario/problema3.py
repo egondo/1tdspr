cf3c3fb9113c1593ed360cfa3ef556b92252405d
Eduardo Gondo <egondo@gmail.com>
2025-03-06 22:48:23 -0300

    Resolução do problema 3 completo com arquivos

--- dicionario/problema3.py
+++ dicionario/problema3.py
@@ -10,18 +10,68 @@ def inclui(conj: list):
     conj.append(prod)
 
 
-def altera():
-    pass
+def altera(conj: list):
+    cod = int(input("Informe o código do produto: "))
+    produto = consulta(conj, cod)
+    if len(produto) > 0:
+        desc = input("Descrição: ")
+        qtd = int(input("Quantidade: "))
+        prc = float(input("Preço: "))
+        produto['descricao'] = desc
+        produto['quantidade'] = qtd
+        produto['preco'] = prc
+    else:
+        print("Nenhum produto com o código fornecido!")
 
-def consulta():
-    pass
+def consulta(conj: list, cod: int) -> dict:
+    for prod in conj:
+        if prod["codigo"] == cod:
+            return prod
+    return {}
 
-def exclui():
-    pass
+def exclui(conj: list):
+    cod = int(input("Informe o código do produto: "))
+    i = 0
+    removido = False
+    while i < len(conj) and not removido:
+        prod = conj[i]
+        if prod['codigo'] == cod:
+            conj.pop(i)
+            removido = True
+        i = i + 1
 
-print("Sistema de cadastro de produtos: ")
+    if removido:
+        print("Produto removido com sucesso!")
+    else:
+        print("Produto nao enontrado")
+
+def gravar_dados(conj: list):
+    with open("dados_produto.txt", mode="w", encoding="utf8") as arq:
+        for prod in conj:
+            cod = prod['codigo']
+            desc = prod['descricao']
+            qtd = prod['quantidade']
+            prc = prod['preco'] 
+            arq.write(f"{cod};{desc};{qtd};{prc}\n")
+
+def ler_dados() -> list:
+    resp = []
+    with open("dados_produto.txt", mode="r", encoding="utf8") as arq:
+        linhas = arq.readlines()
+        for reg in linhas:
+            campos = reg.split(";")
+            prod = {
+                "codigo": int(campos[0]), 
+                "descricao": campos[1],
+                "quantidade": int(campos[2]),
+                "preco": float(campos[3])
+            }
+            resp.append(prod)
+    return resp
+
+print("Sistema de cadastro de produtos: ") 
 opcao = 0
-lista = []
+lista = ler_dados()
 
 while opcao != 5:
     print('1 - inclui')
@@ -33,11 +83,17 @@ while opcao != 5:
     if opcao == 1:
         inclui(lista)
     elif opcao == 2:
-        altera()
+        altera(lista)
     elif opcao == 3:
-        consulta()
+        cod = int(input("Código: "))
+        prod = consulta(lista, cod)
+        if len(prod) > 0:
+            print(prod)
+        else:
+            print("Produto não encontrado")
     elif opcao == 4:
-        exclui()
+        exclui(lista)
 
 
-print(lista)
+#print(lista)
+gravar_dados(lista)
