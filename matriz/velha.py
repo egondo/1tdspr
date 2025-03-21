e5f708edf66d403f862e73e913df18521a21475b
Eduardo Gondo <egondo@gmail.com>
2025-03-20 22:05:33 -0300

    modulo velha.py

---
+++ matriz/velha.py
@@ -0,0 +1,14 @@
+def temEspaco(mat: list) -> bool:
+    pass
+
+def haGanhador(mat: list) -> bool:
+    pass
+
+def joga(mat: list, lin: int, col: int, player: str) -> bool:
+    pass
+
+def criaTabuleiro() -> list:
+    pass
+
+def trocaJogador(jog: str) -> str:
+    pass
