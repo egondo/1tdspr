4eae439abf4d41f06b8f9dcb8d2e190d03877f82
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-14 20:58:30 -0300

    Listas e funcoes

---
+++ listas/exemplo1.py
@@ -0,0 +1,11 @@
+filmes = []
+filmes.append("Snow White")
+filmes.append("Cinderella")
+filmes.append("The Beauty and the Beast")
+filmes.append("The Lion King")
+filmes.append("The Little Mermaid")
+filmes.append("Lilo & Stitch")
+
+for movie in filmes:
+    print(movie)
+
