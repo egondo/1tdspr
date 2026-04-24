694f5ea6565deccfd62238d9c31fc0b095dd9103
Eduardo Gondo <egondo@gmail.com>
2026-04-23 21:58:47 -0300

    Triagem e senha do totem

--- oracle/hospital_TDSPR/banco.py
+++ oracle/hospital_TDSPR/banco.py
@@ -3,6 +3,15 @@ import oracledb
 def get_conexao():
     return oracledb.connect(user="pf0313", password="professor#23", dsn="oracle.fiap.com.br/orcl")
 
+def recupera_senha() -> int:
+    sql = "SELECT senha.nextval FROM dual"
+    with get_conexao() as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql)
+            reg = cur.fetchone()
+            return reg[0]
+
+
 def recupera_paciente_cpf(cpf: str) -> list:
     sql = "SELECT pac_id, nome, convenio, nascimento, cpf FROM PACIENTE WHERE cpf = :cpf"
 
@@ -11,21 +20,45 @@ def recupera_paciente_cpf(cpf: str) -> list:
             cur.execute(sql, {"cpf": cpf})
             pac = cur.fetchone()
             return pac
-        
-def insere_paciente(pac: dict):
-    sql = "INSERT INTO paciente(nome, cpf, convenio, nascimento) VALUES(:nome, :cpf, :convenio, to_date(:nascimento, 'DD-MM-YYYY'))"
+
+def recupera_atendimento_senha(senha: int) -> dict:
+    sql = "SELECT atend_id, chegada, saida, desfecho, senha, pac_id_fk  FROM atendimento WHERE senha = :senha ORDER BY atend_id DESC"
+
     with get_conexao() as conn:
         with conn.cursor() as cur:
+            cur.execute(sql, {"senha": senha})
+            reg = cur.fetchone()
+            atend = {"atend_id": reg[0], "chegada": reg[1], "saida": reg[2], "desfecho": reg[3], "senha": reg[4], "pac_id_fk": reg[5]}
+            return atend
+
+        
+def insere_paciente(pac: dict): 
+    sql = "INSERT INTO paciente(nome, cpf, convenio, nascimento) VALUES(:nome, :cpf, :convenio, to_date(:nascimento, 'DD-MM-YYYY')) RETURNING pac_id INTO :id"
+    with get_conexao() as conn:
+        with conn.cursor() as cur:
+
+            new_var = cur.var(oracledb.NUMBER)
+            pac['id'] = new_var
             cur.execute(sql, pac)
+            pac['id'] = new_var.getvalue()[0]
         conn.commit()
 
 def insere_atendimento(atend: dict):
-    sql = "INSERT INTO atendimento(chegada, saida, desfecho, pac_id_fk) VALUES(to_date(:chegada, 'DD-MM-YYYY HH24:MI'), to_date(:saida, 'DD-MM-YYYY HH24:MI'), :desfecho, :pac_id)"
+    sql = "INSERT INTO atendimento(chegada, saida, desfecho, pac_id_fk, senha) VALUES(to_date(:chegada, 'DD-MM-YYYY HH24:MI'), to_date(:saida, 'DD-MM-YYYY HH24:MI'), :desfecho, :pac_id, :senha)"
 
     with get_conexao() as conn:
         with conn.cursor() as cur:
             cur.execute(sql, atend)
         conn.commit()
+
+def insere_triagem(triagem: dict):
+    sql = "INSERT INTO triagem(data, peso, altura, pressao, batimento, temperatura, sintomas, atend_id_fk, func_id_fk) VALUES(to_date(:data, 'DD-MM-YYYY HH24:MI'), :peso, :altura, :pressao, :batimento, :temperatura, :sintomas, :atend_id_fk, :func_id_fk)"
+
+    with get_conexao() as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql, triagem)
+        conn.commit()
+
 
 
 if __name__ == "__main__":
--- oracle/hospital_TDSPR/negocio.py
+++ oracle/hospital_TDSPR/negocio.py
@@ -1,14 +1,39 @@
 import banco
+import util
 
 #funcionalidade de pegar a senha no totem
 def pega_senha_totem(nome: str, cpf: str) -> int:
     pac = banco.recupera_paciente_cpf(cpf)
+    senha = banco.recupera_senha()
+    chegada = util.get_data_atual()
 
-    if pac: #paciente ja existe na base
-        #inserir o atendimento com os dados do paciente
-        at = {"chegada": '10-04-2026 10:34', 'saida': None, 'desfecho': 'em atendimento', 'pac_id': pac[0]}
-        banco.insere_atendimento(at)
+    if pac == None:
+        pac = {"nome": nome, "cpf": cpf, "convenio": None, "nascimento": None}
+        banco.insere_paciente(pac)
+        at = {"chegada": chegada, 'saida': None, 'desfecho': 'em atendimento', 'pac_id': pac['id'], "senha": senha}
+    else:
+        at = {"chegada": chegada, 'saida': None, 'desfecho': 'em atendimento', 'pac_id': pac[0], "senha": senha}
+    banco.insere_atendimento(at)
+
+    return senha
     
 
+def grava_triagem(triagem: dict, senha: int):
+    data = util.get_data_atual()
+    atend = banco.recupera_atendimento_senha(senha)
+    triagem['atend_id_fk'] = atend['atend_id']
+    triagem['data'] = data
+    banco.insere_triagem(triagem)
+
+def atualiza_paciente_recepcao(paciente: dict, senha: int):
+    pass
+
 if __name__ == "__main__":
-    pega_senha_totem('Milena Brito', '64')
+    #senha = pega_senha_totem('Isa Nunes', '55566677788')
+    #print(senha)
+
+    #senha = pega_senha_totem('Andreia Souza', '3')
+    #print(senha)
+    triagem = {"pressao": "12/8", "batimento": 125, "temperatura": 39, "altura": 1.75, "peso": 68, 'func_id_fk': 1, "sintomas": "vertigem, dor de cabeça, indisposição"}
+
+    grava_triagem(triagem, 18)
