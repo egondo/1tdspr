a4d518c06ed11fb89a055bf847c70ee0bb9f42e3
Eduardo Gondo <egondo@gmail.com>
2026-04-30 23:03:38 -0300

    Sistema hispitalar

--- oracle/hospital_TDSPR/app.py
+++ oracle/hospital_TDSPR/app.py
@@ -3,13 +3,22 @@ from flask_cors import CORS, cross_origin
 import negocio
 
 app = Flask("API Hospital TDSPR")
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/hospital/senha/<cpf>/<nome>", methods=["GET"])
 @cross_origin()
 def get_senha(cpf: str, nome: str):
     senha = negocio.pega_senha_totem(nome, cpf)
     resp = {"senha": senha, "nome": nome}
+    return (resp, 200)
+
+
+@app.route("/hospital/triagem/<int:senha>", methods=["POST"])
+@cross_origin()
+def grava_triagem(senha: int):
+    triagem = request.json
+    negocio.grava_triagem(triagemn, senha)
+    resp = {'title': 'Triagem cadastrada com sucesso', 'status': 200}
     return (resp, 200)
 
 
