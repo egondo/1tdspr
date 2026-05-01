87d83d73a0b4372340e9d866647ba350653b26fd
Eduardo Gondo <egondo@gmail.com>
2026-04-30 22:36:34 -0300

    Finalizando a geracao de senha

--- oracle/hospital_TDSPR/app.py
+++ oracle/hospital_TDSPR/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import negocio
 
-
 app = Flask("API Hospital TDSPR")
+CORS(app)
 
 @app.route("/hospital/senha/<cpf>/<nome>", methods=["GET"])
+@cross_origin()
 def get_senha(cpf: str, nome: str):
     senha = negocio.pega_senha_totem(nome, cpf)
     resp = {"senha": senha, "nome": nome}
