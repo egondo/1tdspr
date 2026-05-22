aa964a5fa979511bdfd0156abb316eb28eb59b7a
Eduardo Gondo <egondo@gmail.com>
2026-05-21 22:48:21 -0300

    finalizando o labirinto

--- labirinto/labirinto.py
+++ labirinto/labirinto.py
@@ -62,3 +62,35 @@ while pos != (0, 0):
     time.sleep(0.8)
 
 #a partir da matriz preenchida, tente encontrar o caminho que devemos percorrer para chegar na saída do labirintop (basta percorrer as posicoes da matriz partindo da posicao (0, 0) e procurando uma casa com valor um a menos do que a posição anterior.
+
+pos = (0, 0)
+caminho = [pos]
+last_ind_lin = len(labirinto) - 1
+last_ind_col = len(labirinto[0]) - 1
+
+while pos != (5, 10):
+    l = pos[0]
+    c = pos[1]
+    valor = labirinto[l][c] - 1
+    #norte
+    if l > 0 and labirinto[l-1][c] == valor:
+        pos = (l-1, c)
+        caminho.append(pos)
+
+    #leste
+    if c < last_ind_col and labirinto[l][c+1] == valor:
+        pos = (l, c+1)
+        caminho.append(pos)
+
+    #sul
+    if l < last_ind_lin and labirinto[l+1][c] == valor:
+        pos = (l + 1, c)
+        caminho.append(pos)
+
+    #oeste
+    if c > 0 and labirinto[l][c - 1] == valor:
+        pos = (l, c - 1)
+        caminho.append(pos)
+
+while len(caminho) > 0:
+    print(caminho.pop())
